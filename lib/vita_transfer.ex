405e6ee583768f6c32ed6a81d3eb2fb3e4122603
John Goff <john.g@hiredhippo.ai>
2021-07-24 15:16:48 -0400

    Initial commit

---
+++ lib/vita_transfer.ex
@@ -0,0 +1,18 @@
+defmodule VitaTransfer do
+  @moduledoc """
+  Documentation for `VitaTransfer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VitaTransfer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vita_transfer_test.exs
@@ -0,0 +1,8 @@
+defmodule VitaTransferTest do
+  use ExUnit.Case
+  doctest VitaTransfer
+
+  test "greets the world" do
+    assert VitaTransfer.hello() == :world
+  end
+end
